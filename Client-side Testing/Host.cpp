9af65fb77c20baefb9cab9d1f54030b05e3d0ea4
Jörg B <34109209+jbar98@users.noreply.github.com>
2019-02-21 22:42:37 +0100

    added sendDeckInfo() // needs to be tested

--- Client-side Testing/Host.cpp
+++ Client-side Testing/Host.cpp
@@ -1,7 +1,26 @@
 #include "Host.h"
 
-/*Host::Host()
+Host::Host(const sf::IpAddress &addressOfOtherGuy, std::vector<CardInfo> *pointerToDeck,
+	std::vector<CardInfo> *pointerToHand)
+	:User{addressOfOtherGuy, pointerToDeck, pointerToHand}
 {
-}*/
+}
 
+void Host::sendDeckInfo()
+{
+	std::vector<sf::Packet> packetsContainingDeckInfo;
 
+	for (int i{ 0 }; i < m_pointerToDeck->size(); ++i)
+	{
+		sf::Packet temp;
+		temp << (*m_pointerToDeck)[i];
+		packetsContainingDeckInfo.push_back(temp);
+	}
+
+	assert(packetsContainingDeckInfo.size() == 32 && "Size of packet vector is not 32!!");
+
+	for (int i{ 0 }; i < packetsContainingDeckInfo.size(); ++i)
+	{
+		m_mySocket.send(packetsContainingDeckInfo[i]);
+	}
+}
--- Client-side Testing/Host.h
+++ Client-side Testing/Host.h
@@ -4,6 +4,9 @@
 class Host : public User
 {
 public:
-	Host();
+	Host(const sf::IpAddress &addressOfOtherGuy, std::vector<CardInfo> *pointerToDeck = nullptr, 
+		std::vector<CardInfo> *pointerToHand = nullptr);
+
+	void sendDeckInfo();
 };
 
