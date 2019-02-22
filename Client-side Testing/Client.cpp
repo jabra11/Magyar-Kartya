5d08c7c8db784d2e023e092ae547a8107edda11a
Jörg B <34109209+jbar98@users.noreply.github.com>
2019-02-22 21:57:46 +0100

    fixed code
    -Made constructor parameteres references instead of pointers

--- Client-side Testing/Client.cpp
+++ Client-side Testing/Client.cpp
@@ -1,8 +1,8 @@
 #include "Client.h"
 
 
-Client::Client(const sf::IpAddress &addressOfOtherGuy, std::vector<CardInfo> *pointerToDeck,
-	std::vector<CardInfo> *pointerToHand)
+Client::Client(const sf::IpAddress &addressOfOtherGuy, std::vector<CardInfo> &pointerToDeck,
+	std::vector<CardInfo> &pointerToHand)
 	:User{addressOfOtherGuy, pointerToDeck, pointerToHand}
 {
 }
@@ -11,12 +11,16 @@ void Client::receiveDeckInfo()
 {
 	std::vector<sf::Packet> packetsContainingTheDeck;
 	
+	int packetsReceivedCounter{ 0 };
 	unsigned const int deckSize{ 32 };
 	for (int i{ 0 }; i < deckSize; ++i)
 	{
 		// receive packets containing the cardInfo of the whole deck and read them into the std::vector
 		sf::Packet temp;
-		m_mySocket.receive(temp);
+		while (m_mySocket.receive(temp) != sf::Socket::Done)
+			std::cerr << "Didn't receive any packet\n";
+
+		std::cout << ++packetsReceivedCounter << ". successfully received packet #" << packetsReceivedCounter << "\n";
 		packetsContainingTheDeck.push_back(temp);
 	}
 
@@ -30,4 +34,15 @@ void Client::receiveDeckInfo()
 	}
 
 	assert(m_copyOfTheDeck.size() == 32 && "CardInfoCopyOfTheDeck is not of size 32.\n");
+}
+
+void Client::printDeck()
+{
+	int counter{ 0 };
+	
+	std::cout << "contents of Deck:\n";
+	for (int i{ 0 }; i < m_copyOfTheDeck.size(); ++i)
+	{
+		std::cout << "#" << ++counter << " " << m_copyOfTheDeck[i].cardRank << " " << m_copyOfTheDeck[i].cardTyp << " " << m_copyOfTheDeck[i].wunschKarte << std::endl;
+	}
 }
--- Client-side Testing/Client.h
+++ Client-side Testing/Client.h
@@ -3,6 +3,7 @@
 
 #include "SFML/Network.hpp"
 #include "User.h"
+#include <iostream>
 
 class Client : public User
 {
@@ -10,10 +11,10 @@ private:
 	std::vector<CardInfo> m_copyOfTheDeck;
 
 public:
-	Client(const sf::IpAddress &addressOfOtherGuy, std::vector<CardInfo> *pointerToDeck = nullptr,
-		std::vector<CardInfo> *pointerToHand = nullptr);
+	Client(const sf::IpAddress &addressOfOtherGuy, std::vector<CardInfo> &pointerToDeck,
+		std::vector<CardInfo> &pointerToHand);
 
 	void receiveDeckInfo();
 
-	
+	void printDeck();
 };
