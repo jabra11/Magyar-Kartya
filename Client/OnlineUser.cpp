764ef03d33562d7d424cb120b2efd79b0e9118c8
Jörg B <34109209+jbar98@users.noreply.github.com>
2019-03-30 19:27:35 +0100

    OnlineUser is now ready to be derived from

--- Client/OnlineUser.cpp
+++ Client/OnlineUser.cpp
@@ -6,7 +6,35 @@ OnlineUser::OnlineUser()
 {
 }
 
-
-OnlineUser::~OnlineUser()
+sf::Packet& operator<<(sf::Packet &packet, const OnlineUser::Default_packet &card)
 {
+	return packet << card.uses_card << card.card_rank << card.card_typ << card.wunschkarte_value;
+}
+
+sf::Packet& operator>>(sf::Packet &packet, OnlineUser::Default_packet &card)
+{
+	return packet >> card.uses_card >> card.card_rank >> card.card_typ >> card.wunschkarte_value;
+}
+
+void OnlineUser::send_card_information(const OnlineUser::Default_packet& packet_to_send)
+{
+	sf::Packet temp;
+	temp << packet_to_send;
+
+	std::cout << "Sending packet..\n";
+	if (m_my_socket.send(temp) == sf::Socket::Done)
+		std::cout << "Successfully sent packet.\n";
+
+	else
+		std::cerr << "ERROR: Could not send packet.\n";
+}
+
+void OnlineUser::receive_card_information(OnlineUser::Default_packet& packet_to_receive)
+{
+	sf::Packet temp;
+	if (m_my_socket.receive(temp) == sf::Socket::Done)
+		std::cout << "Successfully received a packet.\n";
+
+	else
+		std::cerr << "ERROR: Did not receive any packet.\n";
 }
--- Client/OnlineUser.h
+++ Client/OnlineUser.h
@@ -1,9 +1,41 @@
 #pragma once
+
+#include "Card.h"
+#include "Deck.h"
+#include "SFML/Network.hpp"
+
 class OnlineUser
 {
 public:
-	OnlineUser();
-	~OnlineUser();
 
+	struct Default_packet
+	{
+		// User draws a card or uses one.
+		bool uses_card{ false };
+
+		// Initialize with placeholder values.
+		int card_typ{ Card::CardTyp::PLACEHOLDER_TYP };
+		int card_rank{ Card::CardRank::PLACEHOLDER_RANK };
+
+		// Track the wished card typ if used card happens to be a 7.
+		int wunschkarte_value{ Card::CardTyp::PLACEHOLDER_TYP };
+
+	};
+
+protected:
+
+	OnlineUser();
+
+
+	void send_card_information(const Default_packet& packet_to_send);
+	void receive_card_information(Default_packet& packet_to_receive);
+
+	virtual void connect_to_user(sf::IpAddress& target_address) = 0;
+
+	sf::TcpSocket m_my_socket;
+	sf::IpAddress address_of_other_user;
+
+	friend sf::Packet& operator<<(sf::Packet& target, const Default_packet& source);
+	friend sf::Packet& operator>>(sf::Packet& source, const Default_packet& target);
 };
 
