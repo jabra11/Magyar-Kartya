c8e54c62a20135f44ad746e2de245afaf23e6d62
Jörg B <34109209+jbar98@users.noreply.github.com>
2019-03-31 00:46:16 +0100

    added basic host functionality

--- Client/Host.cpp
+++ Client/Host.cpp
@@ -2,11 +2,48 @@
 
 
 
-Host::Host()
+Host::Host(int port)
+	:OnlineUser{ port }
 {
 }
 
-
-Host::~Host()
+bool Host::wait_for_connection(int port)
 {
+	if (m_listener.listen(port) == sf::Socket::Done)
+	{
+		std::cout << "Listening to port " << port << ".\n";
+
+		if (m_listener.accept(m_socket) == sf::Socket::Done)
+		{
+			std::cout << "Successfully connected: " << m_socket.getRemoteAddress() << ".\n";
+			return true;
+		}
+
+		else
+		{
+			std::cerr << "Couldn't establish any connections on port " << port << ".\n";
+			return false;
+		}
+	}
+
+	else
+	{
+		std::cerr << "Couldn't listen to port " << port << ".\n";
+		return false;
+	}
+}
+
+void Host::send_deck_information(Deck& deck_to_send)
+{
+	for (int i{ 0 }; i < deck_to_send.getSize(); ++i)
+	{
+		Default_packet card;
+		card.card_rank = deck_to_send[i].getRank();
+		card.card_typ = deck_to_send[i].getTyp();
+
+		sf::Packet temp;
+		temp << card;
+
+		m_socket.send(temp);
+	}
 }
--- Client/Host.h
+++ Client/Host.h
@@ -1,10 +1,20 @@
 #pragma once
-class Host
+
+#include "OnlineUser.h"
+
+class Host : public OnlineUser
 {
 public:
-	Host();
-	~Host();
 
-	
+	Host(int port);
+
+	// Returns True if a connection has been established
+	bool wait_for_connection(int port);
+
+	void send_deck_information(Deck& deck_to_send);
+
+private:
+
+	sf::TcpListener m_listener;
 };
 
