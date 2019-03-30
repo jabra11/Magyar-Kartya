2da1651c77d361a31ccab5ab5ae6e8e7f4b319f6
Jörg B <34109209+jbar98@users.noreply.github.com>
2019-03-31 00:46:07 +0100

    added basic client functionality

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -2,11 +2,38 @@
 
 
 
-Client::Client()
+Client::Client(int port)
+	:OnlineUser{port}
 {
 }
 
-
-Client::~Client()
+bool Client::connect_to_user(sf::IpAddress& ip_address)
 {
+	if (m_socket.connect(ip_address, m_port) == sf::Socket::Done)
+	{
+		std::cout << "Successfully connected to " << ip_address.getPublicAddress();
+		return true;
+	}
+
+	else
+	{
+		std::cerr << "Couldn't connect to " << ip_address.getPublicAddress();
+		return false;
+	}
+}
+
+void Client::receive_deck_information(Deck& deck_to_copy_in, int size_of_deck)
+{
+	for (int i{ 0 }; i < size_of_deck; ++i)
+	{
+		sf::Packet temp;
+		m_socket.receive(temp);
+
+		Default_packet card;
+		
+		temp >> card;
+		
+		deck_to_copy_in[i].setRank(static_cast<Card::CardRank>(card.card_rank));
+		deck_to_copy_in[i].setTyp(static_cast<Card::CardTyp>(card.card_typ));
+	}
 }
--- Client/Client.h
+++ Client/Client.h
@@ -1,8 +1,19 @@
 #pragma once
-class Client
+
+#include "OnlineUser.h"
+
+class Client : public OnlineUser
 {
 public:
-	Client();
-	~Client();
+	Client(int port);
+
+	// Returns True if a connection has been established
+	bool connect_to_user(sf::IpAddress& ip_address);
+
+	void receive_deck_information(Deck& copy_to_this, int size_of_deck);
+
+private:
+
+	sf::IpAddress m_address_of_other_user;
 };
 
